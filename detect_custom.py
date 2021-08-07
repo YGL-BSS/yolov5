0bf2de32fc34971c9f6a1e68f8fcc5dfd600c208
MarigoldJ <yusang0130@gmail.com>
2021-08-07 22:15:03 +0900

    시연 코드 완료

--- detect_custom.py
+++ detect_custom.py
@@ -15,14 +15,15 @@ from models.experimental import attempt_load
 from utils.datasets import LoadStreams
 from utils.general import check_requirements, check_img_size, check_imshow, set_logging, increment_path, non_max_suppression, \
     scale_coords
+from utils.plots import plot_one_box, colors
 from utils.torch_utils import select_device, time_sync
 
 # custom module
-from utils.ppt import output_to_detect
+from utils.ppt import output_to_detect, EncodeInput, Gesture2Command
 
 @torch.no_grad()
 def run(weights='runs_done/v5m_results/weights/best.pt'):
-    imgsz = 416
+    imgsz = 640
     conf_th = 0.25
     iou_th = 0.45
     max_detect = 1000
@@ -40,7 +41,7 @@ def run(weights='runs_done/v5m_results/weights/best.pt'):
 
     model = attempt_load(w, map_location=device)
     stride = int(model.stride.max())
-    names = models.module.names if hasattr(model, 'module') else model.names
+    names = ['K', 'L', 'paper', 'rock', 'scissor', 'W']
     if half:
         model.half()
     
@@ -59,6 +60,10 @@ def run(weights='runs_done/v5m_results/weights/best.pt'):
     if device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters()))) # run once
     
+    # custom process
+    EI = EncodeInput(20)
+    G2C = Gesture2Command()
+
     t0 = time.time()
     for path, img, im0s, video_cap in dataset:
         img = torch.from_numpy(img).to(device)
@@ -75,6 +80,10 @@ def run(weights='runs_done/v5m_results/weights/best.pt'):
         pred = non_max_suppression(pred, conf_thres=conf_th, iou_thres=iou_th, classes=None, agnostic=False, max_det=max_detect)
         t2 = time_sync()
 
+        # Detected gesture list
+        detected_list = []
+        detected_num = []
+
         for i, detect in enumerate(pred):
             p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
             p = Path(p) # to Path
@@ -82,7 +91,7 @@ def run(weights='runs_done/v5m_results/weights/best.pt'):
             # txt_path
 
             s += '%gx%g ' % img.shape[2:]
-            # gain = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalize gain whwh
+            gain = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalize gain whwh
             # im_cp = im0.copy()    # for save_crop
 
             if len(detect):
@@ -93,26 +102,36 @@ def run(weights='runs_done/v5m_results/weights/best.pt'):
                 for c in detect[:, -1].unique():
                     n = (detect[:, -1] == c).sum()  # detections per class
                     s += f'{n} {names[int(c)]} '    # add to string
+                    detected_num.append(f'{n}')
+                    detected_list.append(names[int(c)])
 
                 # Write results
-                # pass
+                for *xyxy, conf, cls in reversed(detect):
+                    c = int(cls)
+                    label = f'{names[c]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=3)
 
         # Print time (inference + NMS)
-        print(f'{s} Done. ({t2 - t1:.3f}s)', end='\t')
+        # print(f'{s} Done. ({t2 - t1:.3f}s)', end='\t')
 
-        # Get bbox list
-        hand_detected = output_to_detect(s)
-        print(hand_detected)
+        # Get bbox list                     # 전유상 팀원 코드 추가
+        hand_detected = []
+        for n, label in zip(detected_num, detected_list):
+            hand_detected += [label] * int(n)
 
-        # preprocess detected hand signal
-        ####################################
-        # your code
-        ####################################
+        # preprocess detected hand signal   # 정민형 팀원 코드 추가
+        cmd = EI.encode(hand_detected)
+        print(cmd)
 
-        # Action according to the command
-        ####################################
-        # your code
-        ####################################
+        # Action according to the command   # 박도현 팀원 코드 추가
+        if cmd:
+            G2C.activate_command(cmd)
+
+        # show fps
+        cv2.putText(
+            im0, f'FPS: {1/(t2 - t1):.2f}', (10, 50),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2
+        )
 
         # Stream
         cv2.imshow(str(p), im0)
--- utils/ppt.py
+++ utils/ppt.py
@@ -1,6 +1,10 @@
 '''
 gesture 감지 -> 로직 -> 조작
 '''
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
 
 def output_to_detect(s):
     '''
@@ -19,3 +23,129 @@ def output_to_detect(s):
             send_result += [label_dict[int(label[0])]] * int(float(n))
     # print(send_result)
     return send_result
+
+
+class EncodeInput:
+    def __init__(self,alpha):
+        self.alpha = alpha
+                
+        self.hand_count = {
+            'K': 0,
+            'L': 0,
+            'paper': 0,
+            'rock': 0,
+            'scissor': 0,
+            'W': 0
+        }
+        self.none_count = 0
+
+    def reset_count(self):
+        self.hand_count = {
+            'K': 0,
+            'L': 0,
+            'paper': 0,
+            'rock': 0,
+            'scissor': 0,
+            'W': 0
+        }
+        self.none_count = 0
+
+    def accumulate(self,hands):
+        if len(hands) == 1:
+            hand = hands[0]
+            self.hand_count[hand] += 1
+        
+        else: 
+            self.none_count += 1
+        
+
+    def verify(self):
+        output = None
+        if self.none_count == self.alpha:
+            self.reset_count()
+
+        elif max(self.hand_count.values()) == self.alpha:
+            output = max(self.hand_count, key=self.hand_count.get)
+            self.reset_count()
+        
+        return output
+
+    def encode(self,hands):
+        self.accumulate(hands)
+        output = self.verify()
+        if output == None: pass
+        return output
+
+# if __name__ == '__main__':
+#     EI = EncodeInput(10)
+#     while True:
+#         hands = list(input().split())
+#         command = EI.encode(hands)
+#         if command: print(f'==={command}===')
+
+
+class Gesture2Command:
+    gestures = ['K', 'L', 'paper', 'rock', 'scissor', 'W']
+    
+    def __init__(self, gesture = None):
+        self.gesture = gesture
+        self.current_page = -1
+        self.ppt_path = os.path.join(os.getcwd(), 'TestPPT') 
+        self.total_page = self.choosePPT()
+        self.all_ppt = [os.path.join(self.ppt_path, i) for i in self.ppt_list]
+
+    def choosePPT(self):
+        self.ppt_list = os.listdir(self.ppt_path)
+        return len(self.ppt_list)
+
+    def openFirstSlide(self):
+        options = Options()
+        options.add_argument('--start-maximized')
+        self.main_driver = webdriver.Chrome(executable_path="./chromedriver", options = options)
+        self.current_page = 0
+        self.main_driver.get(self.all_ppt[self.current_page])
+
+    def nextSlide(self):
+        if self.current_page < self.total_page - 1:
+            self.current_page += 1
+            self.main_driver.get(self.all_ppt[self.current_page])
+
+    def previousSlide(self):
+        if self.current_page > 1:
+            self.current_page -= 1
+            self.main_driver.get(self.all_ppt[self.current_page])
+
+    def openYouTube(self):
+        options = Options()
+
+        #전체 화면
+        options.add_argument('--start-maximized')
+
+        #pdf 위치
+        get_youtube = "https://www.youtube.com/watch?v=X_jfbXA9mUM"
+
+        self.link_driver = webdriver.Chrome(executable_path="./chromedriver", options = options)
+        self.link_driver.implicitly_wait(1)
+        self.link_driver.get(get_youtube)
+        self.link_driver.find_element_by_css_selector('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button').click()
+    
+    def closeYouTube(self):
+        self.link_driver.quit()
+
+    def endSlide(self):
+        self.main_driver.close()
+    
+    def activate_command(self, gesture):
+        self.gesture = gesture
+        if self.gesture == Gesture2Command.gestures[0]:     # K
+            self.openFirstSlide()
+        elif self.gesture == Gesture2Command.gestures[1]:   # L
+            self.nextSlide()
+        elif self.gesture == Gesture2Command.gestures[2]:   # paper
+            self.previousSlide()
+        elif self.gesture == Gesture2Command.gestures[3]:   # rock
+            self.openYouTube()
+        elif self.gesture == Gesture2Command.gestures[4]:   # scissor
+            self.closeYouTube()
+        elif self.gesture == Gesture2Command.gestures[5]:   # W
+            self.endSlide()
